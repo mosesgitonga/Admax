db4aa130c2b37616c85e2566a4524e297169de9a
mosesgitonga <infosec947@gmail.com>
2024-07-11 14:51:04 +0300

    building the app

---
+++ admax-app/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import IndexPage from './components'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <IndexPage />
+  </React.StrictMode>,
+)
