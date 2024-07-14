f22d257959d6c944bb8227cbda5acb3d0b2a2b7d
mosesgitonga <infosec947@gmail.com>
2024-07-14 11:14:41 +0300

    sitemap changes

--- admax-app/generate-sitemap.js
+++ admax-app/generate-sitemap.js
@@ -1,15 +1,23 @@
 import SitemapGenerator from 'sitemap-generator';
 
-// Create generator
+// Set NODE_TLS_REJECT_UNAUTHORIZED to '1' to enable strict certificate validation
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '1';
+
+// Create a new instance of SitemapGenerator
 const generator = SitemapGenerator('https://admax-tech-solutions.vercel.app', {
-  stripQuerystring: false,
-  filepath: './public/sitemap.xml'  // Specify the output path
+  stripQuerystring: false,  // Optionally keep query parameters in URLs
+ // Specify the output path for the sitemap XML file
 });
 
-// Register event listeners
+// Event listener for when sitemap generation is complete
 generator.on('done', () => {
-  console.log('Sitemap generated!');
+  console.log('Sitemap generated successfully!');
 });
 
-// Start the crawler
+// Event listener for errors during sitemap generation
+generator.on('error', (error) => {
+  console.error('Error generating sitemap:', error);
+});
+
+// Start the crawler to begin generating the sitemap
 generator.start();
